2c0dfd5b9f963411b42b91c07cb2936426b45fdf
EmiKnape <enknape@aol.com>
2018-06-05 19:57:22 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,30 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time>=11 
+  puts "You're in trouble, better get home quick."
 end
+end 
+
+puts simple_curfew_checker(11)
 
 def curfew_checker(time)
-  # code goes here
+  if time>=12
+    puts "You're in trouble get home quick"
 end
+end 
+  puts simple_curfew_checker(12)
 
 def complex_curfew_checker(time)
-  # code goes here
+if time>=9
+puts "Keep having fun!"
 end
+end
+puts complex_curfew_checker(9)
 
 def deluxe_curfew_checker(time)
-  # code goes here
+  num_of_time=6
+puts "I have " + num_of_time. to_s+ "time."
+puts "I have #{num_of_time} time."
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+end 
