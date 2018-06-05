3b7106dda1a4dd7e0cb49a5fdc1b02ef9c9ca4e7
EmiKnape <enknape@aol.com>
2018-06-05 19:48:44 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -21,7 +21,9 @@ end
 puts complex_curfew_checker(9)
 
 def deluxe_curfew_checker(time)
-  
+  num_of_time=6
+puts "I have " + num_of_time. to_s+ "time."
+puts "I have #{num_of_time} time."
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
